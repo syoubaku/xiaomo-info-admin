1a9c61c6f2cf44164908fcfe0441baaf4efff9e8
kohou <hoh_ko@ricoh-japan.co.jp>
2019-07-17 17:38:31 +0900

    clear

--- src/app/pages/xiaomoIndex/components/adminUsers/adminUser.model.ts
+++
@@ -1,8 +0,0 @@
-export class AdminUserModel {
-
-  private name: string;
-
-  constructor(name: string) {
-    this.name = name;
-  }
-}
--- src/app/pages/xiaomoIndex/components/adminUsers/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './adminUserTables.component';
